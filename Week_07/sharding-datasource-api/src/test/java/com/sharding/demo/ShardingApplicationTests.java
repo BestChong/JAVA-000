037891c9d22ae726b61aaf000b79b66b2ee63e64
liuchong <liuchong@bilibili.com>
2020-12-02 11:49:01 +0800

    feat:initialize project

---
+++ Week_07/sharding-datasource-api/src/test/java/com/sharding/demo/ShardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharding.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
